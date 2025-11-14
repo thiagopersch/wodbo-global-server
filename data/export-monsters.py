a1e8f3f0d9c6d47213bc458108a2585a2adc6f97
Peixola <thiagopersch@gmail.com>
2025-11-14 12:59:03 -0400

    Refactor monster export script and remove unused vocation files
    - Cleaned up `export-monsters.py` by removing commented-out code for better readability.
    - Deleted `vocation_windows.txt` and related scripts to streamline the codebase.
    - Updated `Simple Task.xml` to improve task formatting and organization.

--- data/export-monsters.py
+++ data/export-monsters.py
@@ -37,27 +37,3 @@ with open(os.path.join(directory_out, 'monsters.xml'), 'w', encoding='utf-8') as
     f.write(xml_tags)
 
 print('Monsters.xml gerado com sucesso em', directory_out)
-
-# import os
-
-# directory_in = 'C:\\Users\\thiag\\Meus Projetos\\server-tibia\\data\\monster'
-# directory_out = 'C:\\Users\\thiag\\Meus Projetos\\server-tibia\\data\\monster'
-# xml_tags = '<?xml version="1.0" encoding="UTF-8"?>\n<monsters>\n'
-
-# files = [f for f in os.listdir(directory_in) if f.endswith(('.txt', '.xml'))]
-# files.sort()
-
-# for filename in files:
-#     if filename.endswith('.txt'):
-#         name = filename.capitalize().replace('.txt', '')
-#     elif filename.endswith('.xml'):
-#         name = filename.capitalize().replace('.xml', '')
-#         archive = filename.replace('.xml', '')
-#     xml_tags += f'  <monster name="{name}" file="{archive}.xml"/>\n'
-
-# xml_tags += '</monsters>'
-
-# with open(os.path.join(directory_out, 'monsters.xml'), 'w') as f:
-#     f.write(xml_tags)
-
-# print('Monsters.xml gerado com sucesso em', directory_out)
