1d32927ef9b8df3c390a427e46d01c5f04d5c471
Peixola <thiagopersch@gmail.com>
2026-05-21 22:30:36 -0400

    feat: add citizen movement script, update world files, and initialize server logs

--- data/movements/scripts/citizen.lua
+++ data/movements/scripts/citizen.lua
@@ -1,17 +1,25 @@
 function onStepIn(cid, item, pos)
     local thingPos = getThingPos(cid)
-    local magicLevel = 240
+    local magicEffect = 240
+    local towns = {
+        central_city = 2,
+        karakura = 3,
+    }
 
     if isPlayer(cid) == TRUE then
         if (item.actionid == 4036) then
             doPlayerSendTextMessage(cid, 25, "Now you are citizen of Central City.")
-            doSendMagicEffect(thingPos, magicLevel)
-            doPlayerSetTown(cid, 1)
-        end
-        if (item.actionid == 4037) then
+            doSendMagicEffect(thingPos, magicEffect)
+            doPlayerSetTown(cid, towns.central_city)
+            doTeleportThing(cid, getTownTemplePosition(towns.central_city))
+            doSendMagicEffect(getTownTemplePosition(towns.central_city), magicEffect)
+        elseif (item.actionid == 4037) then
             doPlayerSendTextMessage(cid, 25, "Now you are citizen of Karakura.")
-            doSendMagicEffect(thingPos, magicLevel)
-            doPlayerSetTown(cid, 2)
+            doSendMagicEffect(thingPos, magicEffect)
+            doPlayerSetTown(cid, towns.karakura)
+            doTeleportThing(cid, getTownTemplePosition(towns.karakura))
+            doSendMagicEffect(getTownTemplePosition(towns.karakura), magicEffect)
         end
     end
+    return true
 end
